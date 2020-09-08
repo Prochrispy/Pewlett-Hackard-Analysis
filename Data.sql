72cabcecfe0d4013141c04bdba67b35f89e13bda
Prochrispy <68368541+Prochrispy@users.noreply.github.com>
2020-09-07 22:28:35 -0400

    pics

--- Data.sql
+++ Data.sql
@@ -134,11 +134,46 @@ INTO retirement_titles
 FROM employees as e 
 INNER JOIN titles As ti
 ON (e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
-DROP TABLE retirement_titles CASCADE;
-SELECT * FROM retirement_titles
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC;
+DROP TABLE retiring_titles CASCADE;
+SELECT * FROM retirement_titles;
 
-SELECT e.emp_no,
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
 e.first_name,
-e.last_name
+e.last_name,
+ret.title,
+ret.from_date,
+ret.to_date
+INTO unique_titles
+FROM employees as e
+INNER JOIN retirement_titles As ret
+ON (e.emp_no = ret.emp_no)
+WHERE ret.to_date = ('9999-01-01') 
+ORDER BY emp_no ASC;
+SELECT * FROM retiring_titles;
+
+SELECT COUNT(ce.emp_no), ce.title
+INTO retiring_titles
+FROM unique_titles as ce
+GROUP BY ce.title
+ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibilty 
+FROM employees as e
+INNER JOIN dept_emp As de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no; 
+SELECT * FROM mentorship_eligibilty;
+DROP TABLE mentorship_eligibilty CASCADE;
